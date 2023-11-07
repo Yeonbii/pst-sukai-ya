ffbcc56869dbd91549f2e48eceef97e4eb87e3a1
Muhammad Shalahuddin <shalahuddinm002@gmail.com>
2023-11-07 10:08:27 +0800

    Memperbaiki view dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,6 +4,7 @@
 
 // Hamburger dan Sidebar
 const hamburger = document.querySelector('#hamburger');
+const hamburger2 = document.querySelector('#hamburger-2');
 const sidebar = document.querySelector('#sidebar');
 const teksMenu = document.querySelectorAll('#sidebar span');
 const main = document.querySelector('main');
@@ -13,6 +14,8 @@ const adminButton = document.querySelector('#admin-button');
 const adminMenu = document.querySelector('#admin-menu');
 const adminIcon = document.querySelector('#admin-button i');
 
+// Variabel yang akan digunakan untuk action pada tombol Contact
+let isMenuSideClicked = false;
 
 
 // FUNGSI
@@ -21,18 +24,36 @@ const adminIcon = document.querySelector('#admin-button i');
 
 // Fungsi Lebar dan Kecilkan Sidebar
 function luasSidebar() {
-    hamburger.classList.toggle('hamburger-active-dashboard');
-    sidebar.classList.toggle('md:w-[95px]');
-    sidebar.classList.toggle('w-0');
-    sidebar.classList.toggle('w-full');
-    main.classList.toggle('expand-main');
-    main.classList.toggle('contract-main');
+    if (!isMenuSideClicked) {
+        hamburger.classList.toggle('hamburger-active-dashboard');
+        sidebar.classList.toggle('md:w-[95px]');
+        sidebar.classList.toggle('w-0');
+        sidebar.classList.toggle('w-full');
+        main.classList.toggle('expand-main');
+        main.classList.toggle('contract-main');
 
-    setTimeout(() => {
+        setTimeout(() => {
+            teksMenu.forEach(function(element) {
+                element.classList.toggle('hidden');
+            });
+        }, 200); // Tunggu sebentar sebelum menghilangkan hidden
+
+        isMenuSideClicked = true;
+
+    } else {
+
         teksMenu.forEach(function(element) {
             element.classList.toggle('hidden');
         });
-    }, 200); // Tunggu sebentar sebelum menghilangkan hidden
+        hamburger.classList.toggle('hamburger-active-dashboard');
+        sidebar.classList.toggle('md:w-[95px]');
+        sidebar.classList.toggle('w-0');
+        sidebar.classList.toggle('w-full');
+        main.classList.toggle('expand-main');
+        main.classList.toggle('contract-main');
+
+        isMenuSideClicked = false;
+    }
 }
 
 // Fungsi tampil dan tutup Admin Option
@@ -56,6 +77,14 @@ hamburger.addEventListener('click', function() {
     }
 });
 
+hamburger2.addEventListener('click', function() {
+    luasSidebar();
+
+    if (!adminMenu.classList.contains('hidden')) {
+        tampilanAdminOption();
+    }
+});
+
 // Tombol Admin di-klik
 adminButton.addEventListener('click', function() {
     tampilanAdminOption();
@@ -64,46 +93,3 @@ adminButton.addEventListener('click', function() {
         luasSidebar();
     }
 });
-
-// Test Modal
-const openModal = document.getElementById('openModal');
-const closeModal = document.getElementById('closeModal');
-const modal = document.getElementById('myModal');
-
-openModal.addEventListener('click', function() {
-  modal.classList.remove('hidden');
-});
-
-closeModal.addEventListener('click', function() {
-  modal.classList.add('hidden');
-});
-
-// Event listener untuk menutup modal saat wilayah di luar modal content diklik
-document.addEventListener('click', function(event) {
-    if (event.target === modal) {
-      modal.classList.add('hidden');
-    }
-});
-
-// Test Tombol Bulan
-const changeMonthButton = document.getElementById('changeMonthButton');
-const monthYearPicker = document.getElementById('monthYearPicker');
-const monthSelect = document.getElementById('monthSelect');
-const yearSelect = document.getElementById('yearSelect');
-const applyMonthYearButton = document.getElementById('applyMonthYearButton');
-
-changeMonthButton.addEventListener('click', () => {
-    monthYearPicker.classList.toggle('hidden');
-});
-
-applyMonthYearButton.addEventListener('click', () => {
-    const selectedMonth = monthSelect.value;
-    const selectedYear = yearSelect.value;
-    changeMonthButton.textContent = `${getMonthName(selectedMonth)} ${selectedYear}`;
-    monthYearPicker.classList.add('hidden');
-});
-
-function getMonthName(month) {
-    const monthNames = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
-    return monthNames[parseInt(month, 10) - 1];
-}
